f241297cc91b0315ef5430ce41259c6306667119
byronnDev <mikel.e.g.2004@gmail.com>
2025-03-22 02:36:48 +0100

    feat: Agregar función para obtener repositorios anclados de GitHub y soporte para token en variables de entorno

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,9 @@
 /// <reference types="astro/client" />
+interface ImportMetaEnv {
+  readonly GITHUB_TOKEN: string;
+  // más variables de entorno...
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
